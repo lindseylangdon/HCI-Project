1c4c9813b73dfacfac6abdf612c0792e8bdedbe7
Lindsey Langdon <94773899+lindseylangdon@users.noreply.github.com>
2023-12-03 12:51:28 -0600

    revertedserver.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mysql = require('mysql2');
+const mysql = require('mysql');
 const cors = require('cors');
 
 const app = express();
@@ -7,9 +7,9 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-    host: "errordb.cuicv41zhcyy.us-east-1.rds.amazonaws.com",
-    user: "errorDB",
-    password: "sampleDB",
+    host: "localhost",
+    user: "root",
+    password: "sparrow567",
     database: "error-form"
 });
 
@@ -38,18 +38,7 @@ app.post('/submit-report', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 8001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
-process.on('SIGINT', () => {
-    db.end((err) => {
-      if (err) {
-        console.error('Error closing MySQL connection:', err);
-      } else {
-        console.log('MySQL connection closed.');
-        process.exit();
-      }
-    });
-  });
