a60aba6faee4cb80335ab1469f58d7d5134f5149
Lindsey Langdon <94773899+lindseylangdon@users.noreply.github.com>
2023-12-03 12:50:08 -0600

    changed port num

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ app.post('/submit-report', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
